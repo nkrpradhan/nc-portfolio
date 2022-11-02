8ac771c462402e0721b37982c4d356f5fc9e39ff
nkrpradhan <nkrpradhan@gmail.com>
2022-11-02 12:00:29 +0000

    Get Comments Query Fixed for author name

--- models/comments.models.js
+++ models/comments.models.js
@@ -10,7 +10,7 @@ exports.selectCommentsByArticleID = (article_id) => {
     c.comment_id, c.votes, c.created_at, u.name AS author, c.body 
     FROM comments c 
     JOIN articles a ON c.article_id=a.article_id 
-    JOIN users u ON a.author=u.username 
+    JOIN users u ON c.author=u.username 
     WHERE c.article_id=$1
     ORDER BY c.comment_id DESC`;
       return db.query(selectQuery, [article_id]).then(({ rows: comments }) =>
